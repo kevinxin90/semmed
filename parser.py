830479565de84408ffe2351a8181a63ae41fb0b3
Kevin Xin <kevinxin@scripps.edu>
2019-11-15 11:03:16 -0800

    fix unique

--- parser.py
+++ parser.py
@@ -1,7 +1,19 @@
 from collections import defaultdict
 import csv
 import os
+import sys
 
+maxInt = sys.maxsize
+
+while True:
+    # decrease the maxInt value by factor 10 
+    # as long as the OverflowError occurs.
+
+    try:
+        csv.field_size_limit(maxInt)
+        break
+    except OverflowError:
+        maxInt = int(maxInt/10)
 
 def load_data(data_folder):
     nodes_path = os.path.join(data_folder, "nodes_neo4j.csv")
@@ -15,6 +27,7 @@ def load_data(data_folder):
             group_by_semmantic_dict[_item[-2]].append(_item[-1])
             id_type_mapping[_item[-1]] = {'type': _item[-2], 'name': _item[1]}
     disease_related = {}
+    unique_assocs = set()
     with open(edges_path) as f:
         csv_reader = csv.reader(f, delimiter=',')
         next(csv_reader)
@@ -30,8 +43,10 @@ def load_data(data_folder):
                     disease_related[_item[4]][pred] = {}
                 if semantic_type not in disease_related[_item[4]][pred]:
                     disease_related[_item[4]][pred][semantic_type] = []
-                disease_related[_item[4]][pred][semantic_type].append({'pmid': _item[1].split(';'),
-                                                                       'umls': _item[5][5:]})
+                assoc = _item[4] + pred + str(_item[1]) + _item[5]
+                if assoc not in unique_assocs:
+                    unique_assocs.add(assoc)
+                    disease_related[_item[4]][pred][semantic_type].append({'pmid': _item[1].split(';'), 'umls': _item[5][5:]})
             elif _item[5] in group_by_semmantic_dict['disease_or_phenotypic_feature']:
                 if _item[5] not in disease_related:
                     disease_related[_item[5]] = {'_id': _item[5][5:],
@@ -43,7 +58,9 @@ def load_data(data_folder):
                     disease_related[_item[5]][pred] = {}
                 if semantic_type not in disease_related[_item[5]][pred]:
                     disease_related[_item[5]][pred][semantic_type] = []
-                disease_related[_item[5]][pred][semantic_type].append({'pmid': _item[1].split(';'),
-                                                                       'umls': _item[4][5:]})
+                assoc = _item[5] + pred + str(_item[1]) + _item[4]
+                if assoc not in unique_assocs:
+                    unique_assocs.add(assoc)
+                    disease_related[_item[5]][pred][semantic_type].append({'pmid': _item[1].split(';'), 'umls': _item[4][5:]})
     for v in disease_related.values():
         yield v
